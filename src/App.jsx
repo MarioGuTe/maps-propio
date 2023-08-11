6cd7b88e17dd2c310b824351181f9dce80689a87
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-11 18:13:54 -0400

    add paint MenuList and DisplayDifficulty

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-// import s from "./style.module.css";
+import s from "./style.module.css";
+import MenuList from "./components/MenuList/MenuList";
+import DisplayDifficulty from "./components/DisplayDifficulty/DisplayDifficulty";
 
 function App() {
   const [currentDifficulty, setCurrentDifficulty] = useState("");
@@ -11,6 +13,13 @@ function App() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>Select your React difficulty</h1>
+      <div className={s.workspace}>
+        <MenuList
+          difficulty={currentDifficulty}
+          onItemClick={onMenuListItemClick}
+        />
+        <DisplayDifficulty difficulty={currentDifficulty} />
+      </div>
     </div>
   );
 }
