ce64cbd0ffb624da53df45db8d72f0c1233d95e7
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-12 02:00:09 -0400

    add props logic

--- src/components/DisplayDifficulty/DisplayDifficulty.jsx
+++ src/components/DisplayDifficulty/DisplayDifficulty.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import propTypes from "prop-types";
 import s from "./style.module.css";
 
-const DisplayDifficulty = () => {
-  return <div className={s.container}>Hello Im DisplayDifficulty</div>;
+const DisplayDifficulty = (props) => {
+  return (
+    <div className={s.container}>
+      {props.difficulty
+        ? `Difficulty set to: ${props.difficulty}`
+        : "Choose your difficulty setting"}
+    </div>
+  );
 };
 
 export default DisplayDifficulty;
+
+DisplayDifficulty.propTypes = {
+  difficulty: propTypes.string,
+};
