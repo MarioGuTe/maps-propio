f9fe8a57827b527abe9fe3e0a09eccb0a27b51a1
MarioGuTe <mariogutierreztello@gmail.com>
2023-08-12 20:53:49 -0400

    add logic

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import MenuListItem from "../MenuListItem/MenuListItem";
+import { DIFFICULTIES } from "./constants";
+import propTypes from "prop-types";
+import s from "./style.module.css";
 
-const MenuList = () => {
-  return <div>Hello Im MenuList</div>;
+const MenuList = (props) => {
+  return (
+    <div className={s.container}>
+      {DIFFICULTIES.map((difficulty) => (
+        <MenuListItem
+          key={difficulty}
+          onClick={props.onItemClick}
+          difficulty={difficulty}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default MenuList;
+
+MenuList.propTypes = {
+  onItemClick: propTypes.func,
+  difficulty: propTypes.string,
+};
